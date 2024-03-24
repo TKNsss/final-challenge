4b709210f27f385d5518d678279b37494261aa6d
Kim Nguyen <nguyenkimto2004@gmail.com>
2024-03-24 14:21:36 +0700

    style footer main section

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const checked = document.querySelector('.check');
-const nav = document.querySelector('.nav');
+const checked = document.querySelector(".check");
+const nav = document.querySelector(".nav");
 
-checked.addEventListener('click', () => {
-  nav.classList.toggle(['nav--visible']);
-})
+checked.addEventListener("click", () => {
+  nav.classList.toggle(["nav--visible"]);
+});
+
+document.querySelectorAll(".btn-toggle").forEach((button) => {
+  button.addEventListener("click", () => {
+    const targetId = button.dataset.target;
+    const targetContent = document.getElementById(targetId);
+    targetContent.classList.toggle("footer__content--visible");
+  });
+});
